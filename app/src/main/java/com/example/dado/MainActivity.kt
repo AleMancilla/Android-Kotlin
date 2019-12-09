5b69a09e02b9bdab7ff970dd431172427cefa1ff
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2019-12-09 00:44:08 -0400

    Kotlin evento interface Text Wacher.. textChangedListener .. captura datos de donde lo asignemos con 3 funciones *antes *durante * despues

--- app/src/main/java/com/example/dado/MainActivity.kt
+++ app/src/main/java/com/example/dado/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.dado
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.EditText
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextWatcher {
+
 
     private var EditName : EditText? = null // en Kotlin no se permiten objetos nulos por lo que lo forzamos con esta linea
     // en kotlin no es necesario utilizar punto y coma
@@ -18,8 +22,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        EditName = findViewById(R.id.editText_edad) as EditText
+        EditName = findViewById(R.id.editText_nombre) as EditText
         EditAge = findViewById(R.id.editText_edad) as EditText
 
+        // evento interface Text Wacher.. textChangedListener .. captura datos de donde lo asignemos
+        EditName?.addTextChangedListener(this)
+        EditAge?.addTextChangedListener(this)
+
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    override fun afterTextChanged(s: Editable?) {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        Toast.makeText(this, s.toString()+"_"+start.toString()+"_"+before.toString()+"_"+count.toString() , Toast.LENGTH_SHORT).show()
     }
 }
