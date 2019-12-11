5aa64fd545ce0d929167a0e09a5fcd6ed4e7af66
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2019-12-11 19:43:13 -0400

    Funcion OneClickListener

--- app/src/main/java/com/example/dado/MainActivity.kt
+++ app/src/main/java/com/example/dado/MainActivity.kt
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.IntegerRes
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), TextWatcher {
+class MainActivity : AppCompatActivity(), TextWatcher, View.OnClickListener {
+
 
 
     private var EditName : EditText? = null // en Kotlin no se permiten objetos nulos por lo que lo forzamos con esta linea
@@ -24,6 +27,8 @@ class MainActivity : AppCompatActivity(), TextWatcher {
     // en kotlin no es necesario que deblare el tipo de variable... basta con ponerle el dato y kotlin interpretara que tipo de dato es
     // la unica diferencia es que se pone = para poner el dato , caso contrario se pone :
 
+    private var boton : Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,14 +38,17 @@ class MainActivity : AppCompatActivity(), TextWatcher {
         TextViewName = findViewById(R.id.textView_Name) as TextView
         TextViewAge = findViewById(R.id.textView_Age) as TextView
 
+        boton = findViewById(R.id.button_ejecutar) as Button
+
+        boton!!.setOnClickListener(this) // recibe  un objetp
 
         // evento interface Text Wacher.. textChangedListener .. captura datos de donde lo asignemos
         EditName!!.addTextChangedListener(this) // con la doble admiracion se asegura que esa variable no contiene nulo, caso contrario da error
-        EditAge?.addTextChangedListener(this) // con la interrogacion dice que podra recibir un dato nulo pero no necesariamente
+        EditAge!!.addTextChangedListener(this) // con la interrogacion dice que podra recibir un dato nulo pero no necesariamente
 
     }
 
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////funciones de Text Listener//////////////////////////////////////////////////////
 
     override fun afterTextChanged(s: Editable?) {
         //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
@@ -52,6 +60,28 @@ class MainActivity : AppCompatActivity(), TextWatcher {
 
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
         //Toast.makeText(this, s.toString()+"_"+start.toString()+"_"+before.toString()+"_"+count.toString() , Toast.LENGTH_SHORT).show()
+       // name = EditName?.text.toString()
+       // age = EditAge?.text.toString()
+//
+       // if(name != "")
+       // {
+       //     textView_Name?.text = name// podemos obtener datos o asignar datos
+       // }
+       // if(age != "")
+       // {
+       //     textView_Age?.text = age
+       // }
+
+
+    }
+
+    ////////////////////////////Evento On Click///////////////////////////////////////////
+    override fun onClick(v: View?) { //con esto "v" podemos obtener los ID o elementos que estan generando el evento Onclick
+        operacion()
+    }
+    ////////////////////funciones///////////////////////////////////////////
+    private fun operacion()
+    {
         name = EditName?.text.toString()
         age = EditAge?.text.toString()
 
@@ -63,7 +93,6 @@ class MainActivity : AppCompatActivity(), TextWatcher {
         {
             textView_Age?.text = age
         }
-
-
     }
+
 }
